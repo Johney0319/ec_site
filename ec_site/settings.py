2d5efe2b91ec2039d61e1098e985e393f0eae667
naka0303 <jyunki.nakagawa@gmail.com>
2021-03-16 19:48:43 +0900

    settings 不要コードの削除

--- ec_site/settings.py
+++ ec_site/settings.py
@@ -119,13 +119,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
 
 # 画像保存先の指定
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -138,7 +135,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
 
 AUTH_USER_MODEL = 'ec.CustomUser'
 
