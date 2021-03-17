d8b1288d9d43f47caa37d172b6a2d76ab69a3405
naka0303 <jyunki.nakagawa@gmail.com>
2021-03-17 17:19:28 +0900

    views.py debug処理

--- ec/views.py
+++ ec/views.py
@@ -397,7 +397,7 @@ def purchase_history(request):
         'purchase_history_user_len': len(purchase_history_user)
     }
 
-    logging.DEBUG('--------------------------------', purchase_history_user)
+    logging.DEBUG(purchase_history_user)
 
     return render(request, 'purchase_history.html', params)
 
