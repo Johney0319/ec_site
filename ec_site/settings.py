04f8abd0bd4a6d932f158a12a5e05307ebaff392
naka0303 <jyunki.nakagawa@gmail.com>
2021-03-17 14:51:40 +0900

    ediaディレクトリの削除

--- ec_site/settings.py
+++ ec_site/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # 画像保存先の指定
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
