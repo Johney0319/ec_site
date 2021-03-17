d0bfd923e4c9ea80893c1ca914fbbe233f2f6b25
naka0303 <jyunki.nakagawa@gmail.com>
2021-03-18 00:10:20 +0900

    settings変更

--- ec_site/local_settings.py
+++ ec_site/local_settings.py
@@ -1,10 +1,21 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ec_site',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '',
+        'POST': '',
     }
 }
 
