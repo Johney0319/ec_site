97c0aef8d41e35855f7a1d38950ce8ea41de5d8d
naka0303 <jyunki.nakagawa@gmail.com>
2021-03-17 15:02:41 +0900

    ディレクトリ構造変更

--- ec_site/settings.py
+++ ec_site/settings.py
@@ -122,13 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 # 画像保存先の指定
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_URL = '/media/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'static'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
