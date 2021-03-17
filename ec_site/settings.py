2b1c85d9036c06b8a9b800c6db767f4aee3183bb
naka0303 <jyunki.nakagawa@gmail.com>
2021-03-17 14:38:56 +0900

    static改修

--- ec_site/settings.py
+++ ec_site/settings.py
@@ -128,13 +128,14 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # staticの読み込み
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 
 AUTH_USER_MODEL = 'ec.CustomUser'
 
